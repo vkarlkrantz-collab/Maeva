422beeb98add5bbe3b174bd7d1bc43e0446ebfb0
Ella Eriksson <ella.eriksson2002@gmail.com>
2025-10-16 19:32:03 +0200

    16/10 1.2

--- header.js
+++ header.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("Header.html")
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById("header-placeholder").innerHTML = data;
-      });
-  });
+  fetch("Header.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("header-placeholder").innerHTML = data;
 
-  
+      const hamburger = document.querySelector(".hamburger");
+      const meny = document.querySelector(".meny");
+
+      if (hamburger && meny) {
+        hamburger.addEventListener("click", () => {
+          meny.classList.toggle("open");
+        });
+      }
+    });
+});
